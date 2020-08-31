8531de49039eef8a2820a32be47910dbd49ec9ad
arlenvargas <arlenvargas2000@gmail.com>
2020-08-30 19:40:24 -0600

    add useEffect to get members in locals

--- src/views/local/MemberForm.js
+++ src/views/local/MemberForm.js
@@ -1,25 +1,40 @@
 import React, { useContext, useState, useEffect } from 'react';
 import WizardInput from '../components/WizardInput';
+import { isIterableArray } from '../../template/helpers/utils';
+import MemberAction from '../../stores/member/MemberAction';
 import Select from 'react-select';
 import { selectMembersOptions } from '../../selectors/members/MemberSelectors';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { LocalContext } from '../context';
 
 const MemberForm = ({ register, errors }) => {
+  const dispatch = useDispatch();
+
+  const [memberElected, setMemberElected] = useState({});
+
   const { handleInputChangeLocal } = useContext(LocalContext);
 
   const [memberId, setMemberId] = useState('');
+
   const members = useSelector(selectMembersOptions);
 
   const memberObjetive = useSelector(state => state.members);
-  const [memberSelected] = memberObjetive.filter(x => x.id === memberId);
+
+  useEffect(() => {
+    if (isIterableArray(members)) {
+      const [memberSelected] = memberObjetive.filter(x => x.id === memberId);
+      setMemberElected(memberSelected);
+    } else {
+      dispatch(MemberAction.getMembers());
+    }
+  }, []);
 
   useEffect(() => {
     handleInputChangeLocal({
       name: 'member',
-      value: memberSelected
+      value: memberElected
     });
-  }, [memberId]);
+  }, [memberId, members]);
 
   return (
     <>
