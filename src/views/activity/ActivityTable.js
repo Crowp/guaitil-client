977d5c31730374987640c13899dcb02f456d621c
itsRicmor <ricardojms1999@gmail.com>
2020-09-02 13:43:29 -0600

    Delete Activity by id

--- src/views/activity/ActivityTable.js
+++ src/views/activity/ActivityTable.js
@@ -1,12 +1,23 @@
 import React, { createRef, Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Card, CardBody, CustomInput, InputGroup, Modal } from 'reactstrap';
+import {
+  Button,
+  Card,
+  CardBody,
+  CustomInput,
+  InputGroup,
+  Modal,
+  ModalHeader,
+  ModalFooter,
+  ModalBody
+} from 'reactstrap';
 import FalconCardHeader from '../components/common/FalconCardHeader';
 import ButtonIcon from '../components/common/ButtonIcon';
 import { Table } from '../components/tables';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { PhoneFormatter, ActionFormatter } from '../components/tables/formatters';
+import { ActionFormatter } from '../components/tables/formatters';
+import ActivityAction from '../../stores/activity/ActivityAction';
 
 const columnsDefault = (onEditCell, onDeleteCell) => [
   {
@@ -55,12 +66,18 @@ const columnsDefault = (onEditCell, onDeleteCell) => [
 
 const ActivityTable = ({ activities, title, all = false }) => {
   let table = createRef();
+  const [idActivity, setIdActivity] = useState(false);
   const [isSelected, setIsSelected] = useState(false);
   const history = useHistory();
   const dispatch = useDispatch();
 
   const onDeleteCell = id => {
-    console.log(id);
+    setIdActivity(id);
+  };
+
+  const onDeleteModal = () => {
+    dispatch(ActivityAction.deleteActivity(idActivity));
+    setIdActivity(false);
   };
 
   const onEditCell = id => {
@@ -83,43 +100,55 @@ const ActivityTable = ({ activities, title, all = false }) => {
     totalSize: activities.length
   };
   return (
-    <Card className="mb-3">
-      <FalconCardHeader title={title} light={false}>
-        {isSelected ? (
-          <InputGroup size="sm" className="input-group input-group-sm">
-            <CustomInput type="select" id="bulk-select">
-              <option>Bulk actions</option>
-              <option value="Delete">Delete</option>
-              <option value="Archive">Archive</option>
-            </CustomInput>
-            <Button color="falcon-default" size="sm" className="ml-2">
-              Apply
-            </Button>
-          </InputGroup>
-        ) : (
-          <Fragment>
-            <ButtonIcon
-              icon="plus"
-              transform="shrink-3 down-2"
-              color="falcon-default"
-              size="sm"
-              onClick={() => history.push('/activities/create')}
-            >
-              New
-            </ButtonIcon>
-            <ButtonIcon icon="filter" transform="shrink-3 down-2" color="falcon-default" size="sm" className="mx-2">
-              Filter
-            </ButtonIcon>
-            <ButtonIcon icon="external-link-alt" transform="shrink-3 down-2" color="falcon-default" size="sm">
-              Export
-            </ButtonIcon>
-          </Fragment>
-        )}
-      </FalconCardHeader>
-      <CardBody className="p-0">
-        <Table reference={table} options={options} columns={columns} items={activities} onSelect={onSelect} />
-      </CardBody>
-    </Card>
+    <>
+      <Card className="mb-3">
+        <FalconCardHeader title={title} light={false}>
+          {isSelected ? (
+            <InputGroup size="sm" className="input-group input-group-sm">
+              <CustomInput type="select" id="bulk-select">
+                <option>Bulk actions</option>
+                <option value="Delete">Delete</option>
+                <option value="Archive">Archive</option>
+              </CustomInput>
+              <Button color="falcon-default" size="sm" className="ml-2">
+                Apply
+              </Button>
+            </InputGroup>
+          ) : (
+            <Fragment>
+              <ButtonIcon
+                icon="plus"
+                transform="shrink-3 down-2"
+                color="falcon-default"
+                size="sm"
+                onClick={() => history.push('/activities/create')}
+              >
+                New
+              </ButtonIcon>
+              <ButtonIcon icon="filter" transform="shrink-3 down-2" color="falcon-default" size="sm" className="mx-2">
+                Filter
+              </ButtonIcon>
+              <ButtonIcon icon="external-link-alt" transform="shrink-3 down-2" color="falcon-default" size="sm">
+                Export
+              </ButtonIcon>
+            </Fragment>
+          )}
+        </FalconCardHeader>
+        <CardBody className="p-0">
+          <Table reference={table} options={options} columns={columns} items={activities} onSelect={onSelect} />
+        </CardBody>
+      </Card>
+      <Modal isOpen={!!idActivity} toggle={() => setIdActivity(false)}>
+        <ModalHeader>Eliminar Multimedia</ModalHeader>
+        <ModalBody>¿Desea eliminar este archivo multimedia?</ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={() => setIdActivity(false)}>
+            Cancelar
+          </Button>
+          <Button onClick={onDeleteModal}>Eliminar</Button>
+        </ModalFooter>
+      </Modal>
+    </>
   );
 };
 
