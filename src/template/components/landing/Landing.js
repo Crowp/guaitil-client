ba252eec6a715ecf955c3ca3988442e0e13aa941
tatimoramen <tatimorales02@icloud.com>
2020-09-09 19:00:10 -0600

    medio carrousel landing

--- src/template/components/landing/Landing.js
+++ src/template/components/landing/Landing.js
@@ -5,17 +5,18 @@ import Banner from './Banner';
 import Processes from './Processes';
 import Cta from './Cta';
 import FooterStandard from './FooterStandard';
-import Slider from 'react-slick';
+import LocalImage from './components/LocalImages';
 
 const Landing = ({ location, match }) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
+
   return (
     <Fragment>
       <NavbarStandard location={location} match={match} />
       <Banner />
-      <Slider />
+      <LocalImage tittle="Talleres" />
       <Processes />
       <Cta />
       <FooterStandard />
