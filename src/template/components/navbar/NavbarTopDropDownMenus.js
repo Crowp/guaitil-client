79d3ebe4766ee96bbc205e31122258c5d2c9de93
Luis <luis.rodriguez.baltodano@est.una.ac.cr>
2020-09-13 21:27:11 -0600

    ..

--- src/template/components/navbar/NavbarTopDropDownMenus.js
+++ src/template/components/navbar/NavbarTopDropDownMenus.js
@@ -2,27 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { NavItem, NavLink } from 'reactstrap';
+import '../../../template/assets/styles-css/style-landing/landing.css';
 
 const NavbarTopDropDownMenus = () => {
   return (
     <>
       <NavItem>
-        <NavLink tag={Link} to="/">
+        <NavLink style={{ color: 'white', letterSpacing: '.5px' }} tag={Link} to="/">
           Home
         </NavLink>
       </NavItem>
       <NavItem>
-        <NavLink tag={Link} to="#!">
+        <NavLink style={{ color: 'white', letterSpacing: '.5px' }} tag={Link} to="#!">
           Talleres
         </NavLink>
       </NavItem>
       <NavItem>
-        <NavLink tag={Link} to="#!">
+        <NavLink style={{ color: 'white', letterSpacing: '.5px' }} tag={Link} to="#!">
           Cocinas
         </NavLink>
       </NavItem>
       <NavItem>
-        <NavLink tag={Link} to="#!">
+        <NavLink style={{ color: 'white', letterSpacing: '.5px' }} tag={Link} to="#!">
           Actividades
         </NavLink>
       </NavItem>
