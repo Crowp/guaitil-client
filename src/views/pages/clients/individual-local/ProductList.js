6184cd974f82851cffd8139516d0871c0d7c66ed
Luis <luis.rodriguez.baltodano@est.una.ac.cr>
2020-10-14 16:36:59 -0600

    ..

--- src/views/pages/clients/individual-local/ProductList.js
+++ src/views/pages/clients/individual-local/ProductList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { isIterableArray } from '../../../../template/helpers/utils';
-import workshop from '../../../../template/assets/img/background/local.jpg';
+import workshop from '../../../../template/assets/img/background/WorshopImage.jpg';
 import Slider from 'react-slick/lib';
 
 const sliderSettings = {
