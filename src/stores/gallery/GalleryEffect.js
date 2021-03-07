1ec2ed9758bab7a4f1d854f17a16e1e0fa637535
itsRicmor <ricardojms1999@gmail.com>
2021-03-06 18:23:52 -0600

    create gallery requests

--- src/stores/gallery/GalleryEffect.js
+++ src/stores/gallery/GalleryEffect.js
@@ -1,25 +1,19 @@
 import environment from 'environment';
-import * as EffectUtility from '../../utils/EffectUtility';
 import HttpErrorResponseModel from '../../models/HttpErrorResponseModel';
-import GalleryModel from '../../models/GalleryModel';
-import * as MultimediaEffect from '../multimedia/MultimediaEffect';
+import { createGalleryFilesPostRequest } from './requests/GalleryFilesPostRequest';
+import { createGalleryDeleteFilesByIdRequest } from './requests/GalleryDeleteFilesByIdRequest';
+import { createGalleryImagesRequest } from './requests/GalleryImagesRequest';
 
 export const requestGalery = async () => {
   const endpoint = environment.api.gallery;
-  return await EffectUtility.getToModel(GalleryModel, endpoint);
+  return await createGalleryImagesRequest().getResponse();
 };
 
 export const requestDeleteGaleryMultimedia = async id => {
-  const endpoint = environment.api.gallery + '/' + id;
-  const response = await EffectUtility.deleteToModel(GalleryModel, endpoint);
+  const response = createGalleryDeleteFilesByIdRequest(id).getResponse();
   return response instanceof HttpErrorResponseModel ? response : id;
 };
 
 export const requestAddMultimedia = async multimedia => {
-  const endpoint = environment.api.gallery;
-  let response = await MultimediaEffect.requestCreateMultimediaList(multimedia, 'gallery_', '_image');
-  if (response instanceof HttpErrorResponseModel) {
-    return response;
-  }
-  return await EffectUtility.postToModel(GalleryModel, endpoint, { multimedia: response });
+  return await createGalleryFilesPostRequest(multimedia).getResponse();
 };
