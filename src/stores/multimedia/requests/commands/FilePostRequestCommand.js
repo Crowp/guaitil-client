4b898ed87900216c84d4a46404a7d6c7c67da887
itsRicmor <ricardojms1999@gmail.com>
2021-03-07 19:10:06 -0600

    add images compress

--- src/stores/multimedia/requests/commands/FilePostRequestCommand.js
+++ src/stores/multimedia/requests/commands/FilePostRequestCommand.js
@@ -3,6 +3,7 @@ import { RollbackRequestCommand } from '../../../../utils/requests/commands/Roll
 import { createFileDeleteByIdRequestCommand } from './FileDeleteByIdRequestCommand';
 import MultimediaModel from '../../../../models/MultimediaModel';
 import * as EffectUtility from '../../../../utils/EffectUtility';
+import imageCompression from 'browser-image-compression';
 
 export class FilePostRequestCommand extends RollbackRequestCommand {
   constructor(file, prefix, suffix) {
@@ -13,7 +14,7 @@ export class FilePostRequestCommand extends RollbackRequestCommand {
   }
   executeRequest = async () => {
     const endpoint = environment.api.multimedia.replace(':id', 'upload');
-    const formData = createFileFormData(this.file, this.prefix, this.suffix);
+    const formData = await createFileFormData(this.file, this.prefix, this.suffix);
 
     const response = await EffectUtility.postToModel(MultimediaModel, endpoint, formData);
 
@@ -34,24 +35,29 @@ export const createFilePostRequestCommand = (files, prefix, suffix) => {
   return new FilePostRequestCommand(files, prefix, suffix);
 };
 
-const createFile = multimedia => {
-  let byteString = atob(multimedia.base64.split(',')[1]);
-  let ab = new ArrayBuffer(byteString.length);
-  let ia = new Uint8Array(ab);
-
-  for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-  const blob = new Blob([ab], { type: multimedia.type });
-  return new File([blob], multimedia.path, { type: multimedia.type });
+const createFile = async multimedia => {
+  const compressedBlob = await compressImageFile(multimedia);
+  return new File([compressedBlob], multimedia.path, { type: multimedia.type });
 };
 
-const createFileFormData = (multimedia, prefix, suffix) => {
-  const file = createFile(multimedia);
+const createFileFormData = async (multimedia, prefix, suffix) => {
+  const file = await createFile(multimedia);
+  debugger;
+  console.log(file);
   const formData = new FormData();
   formData.append('file', file);
   formData.append('prefix', prefix);
   formData.append('suffix', suffix);
   formData.append('type', multimedia.type === 'image/jpeg' ? 'IMAGE' : 'VIDEO');
   return formData;
+};
+
+const compressImageFile = async multimedia => {
+  const options = {
+    maxSizeMB: 1,
+    maxWidthOrHeight: 1920,
+    useWebWorker: true
+  };
+  const file = await imageCompression.getFilefromDataUrl(multimedia.base64, multimedia.path);
+  return await imageCompression(file, options);
 };
