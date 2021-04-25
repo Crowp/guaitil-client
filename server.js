82429b4ae51c1acf55237b7a4cf19944416e09c9
itsRicmor <ricardojms1999@gmail.com>
2021-04-24 21:39:00 -0600

    change api port

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(80, function() {
+app.listen(3000, function() {
   console.log(`Frontend start`);
 });
