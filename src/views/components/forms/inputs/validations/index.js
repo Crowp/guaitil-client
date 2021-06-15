cea4f9a1c0ceed4df50478ee352944d4ca9e5cf5
arlenvargas2000@gmail.com <arlenvargas2000@gmail.com>
2021-06-14 19:28:59 -0600

    only letters en formulario de miembro

--- src/views/components/forms/inputs/validations/index.js
+++ src/views/components/forms/inputs/validations/index.js
@@ -1,4 +1,5 @@
 export const whitespacesValidation = value => !!value.trim() || 'No se permiten espacios en blanco';
+
 export const maskInputEmptyValidation = value => (/^[_-]+$/.test(value) ? 'Campo obligatorio' : true);
 
 export const emailRegexPattern = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
@@ -8,3 +9,5 @@ export const phoneRegexPattern = /^[0-9]{8}$/;
 export const dniRegexPattern = /^[0-9]{9}$/;
 
 export const noNumbersPattern = /^([^0-9]*)$/;
+
+export const onlyLettersPattern = /^[^$%*1234567890+~"'&|<>#]*$/;
